d7eb4be5e7a7723791523a7d7fe45c5d83773cb2
Dragos Sarpe <contact@usdgadget.com>
2018-09-14 19:58:21 +0300

    Added models

---
+++ src/main/java/com/dgs/springbootmongodb/models/Address.java
@@ -0,0 +1,19 @@
+package com.dgs.springbootmongodb.models;
+
+public class Address {
+
+    private String city;
+    private String country;
+
+    protected Address() {
+
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
